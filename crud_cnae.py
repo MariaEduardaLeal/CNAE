8159bbb65e81ece0817066a14766b7ef2d42b4ca
MariaEduardaLeal <eduardaleal753@gmail.com>
2024-03-23 09:30:29 -0300

    Adicionando verificação se já existi cnae

--- crud_cnae.py
+++ crud_cnae.py
@@ -1,8 +1,24 @@
 def inserir_dados(conexao, cnae, descricao_principal, anexo, fator_r, aliquota, contabilizei):
     cursor = conexao.cursor()
-    sql = "INSERT INTO tabela_cnae (cnae, descricao_principal, anexo, fator_r, aliquota, contabilizei) VALUES (%s, %s, %s, %s, %s, %s)"
-    val = (cnae, descricao_principal, anexo, fator_r, aliquota, contabilizei)
-    cursor.execute(sql, val)
-    conexao.commit()
-    print(cursor.rowcount, "registro(s) inserido(s).")
+
+    # Verificar se o dado já existe na tabela
+    cursor.execute("SELECT cnae FROM tabela_cnae WHERE cnae = %s", (cnae,))
+    resultado = cursor.fetchone()
+
+    if resultado:
+        # Atualizar os dados na tabela
+        sql = "UPDATE tabela_cnae SET descricao_principal = %s, anexo = %s, fator_r = %s, aliquota = %s, contabilizei = %s WHERE cnae = %s"
+        val = (descricao_principal, anexo, fator_r, aliquota, contabilizei, cnae)
+        cursor.execute(sql, val)
+        conexao.commit()
+        print(f"Dados do CNAE {cnae} atualizados com sucesso.")
+    else:
+        # Inserir os dados na tabela
+        sql = "INSERT INTO tabela_cnae (cnae, descricao_principal, anexo, fator_r, aliquota, contabilizei) VALUES (%s, %s, %s, %s, %s, %s)"
+        val = (cnae, descricao_principal, anexo, fator_r, aliquota, contabilizei)
+        cursor.execute(sql, val)
+        conexao.commit()
+        print(f"Dados do CNAE {cnae} inseridos com sucesso.")
+
+    # Fechar o cursor
     cursor.close()
