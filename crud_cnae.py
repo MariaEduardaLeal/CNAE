453a32a7d45eadc258a6d15f3684ee09b846d590
MariaEduardaLeal <eduardaleal753@gmail.com>
2024-03-24 16:30:28 -0300

    Revert "Inserir dados apenas do cnae"
    This reverts commit 29d00548f15ac606a5336249b962e81c5ec56d1f.
    
    ssfsdf

--- crud_cnae.py
+++ crud_cnae.py
@@ -48,29 +48,4 @@ def inserir_dados_hierarquia(conexao, cnae, divisao, grupo, classe, subclasse):
     # Fechar o cursor
     cursor.close()
 
-def inserir_dados_cnae(conexao, cnae, descricao_principal, anexo, fator_r, aliquota, contabilizei):
-    cursor = conexao.cursor()
 
-    # Verificar se os dados já existem no banco de dados
-    sql_select = "SELECT * FROM tabela_cnae WHERE cnae = %s AND aliquota = %s AND anexo = %s"
-    val_select = (cnae, aliquota, anexo)
-    cursor.execute(sql_select, val_select)
-    resultado = cursor.fetchone()
-
-    if resultado:
-        # Se os dados já existirem, execute a atualização
-        sql_update = "UPDATE tabela_cnae SET descricao_principal = %s, fator_r = %s, contabilizei = %s WHERE cnae = %s AND aliquota = %s AND anexo = %s"
-        val_update = (descricao_principal, fator_r, contabilizei, cnae, aliquota, anexo)
-        cursor.execute(sql_update, val_update)
-        conexao.commit()
-        print(f"Dados do CNAE {cnae} atualizados com sucesso.")
-    else:
-        # Se os dados não existirem, execute a inserção
-        sql_insert = "INSERT INTO tabela_cnae (cnae, descricao_principal, anexo, fator_r, aliquota, contabilizei) VALUES (%s, %s, %s, %s, %s, %s)"
-        val_insert = (cnae, descricao_principal, anexo, fator_r, aliquota, contabilizei)
-        cursor.execute(sql_insert, val_insert)
-        conexao.commit()
-        print(f"Dados do CNAE {cnae} inseridos com sucesso.")
-
-    # Fechar o cursor
-    cursor.close()
